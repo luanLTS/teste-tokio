b47e4763f4677071b1d65318ac107796a177fd73
Luan Teixeira <luanlts@outlook.com>
2025-03-19 01:17:14 -0300

    feat(ClientController): add enpoint put para atualizar cliente

--- api-java/src/main/java/br/com/tokiomarine/seguradora/Controller/ClientController.java
+++ api-java/src/main/java/br/com/tokiomarine/seguradora/Controller/ClientController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.tokiomarine.seguradora.model.Client;
 import br.com.tokiomarine.seguradora.repository.IAddressRepository;
 import br.com.tokiomarine.seguradora.repository.IClientRepository;
+import br.com.tokiomarine.seguradora.utils.Utils;
 
 @RestController
 @RequestMapping("/api/client")
@@ -46,6 +49,29 @@ public class ClientController {
     @GetMapping("/")
     public ResponseEntity<List<Client>> listClients () {
         return ResponseEntity.ok(repository.findAll());
+    }
+
+
+    @PutMapping("/{idClient}")
+    public ResponseEntity<?> updateClient(@RequestBody Client newInfosClient, @PathVariable Long idClient) {
+        var client = repository.findById(idClient).orElse(null);
+
+        if(client == null) 
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não encontrado");
+
+        var clientEmail = repository.findByEmail(newInfosClient.getEmail());
+
+        if(clientEmail!=null && !clientEmail.getId().equals(client.getId()))
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Este email não pode ser utilizado");
+
+        newInfosClient.setId(client.getId());
+
+        Utils.copyNonNullProperties(newInfosClient, client);
+
+        var clientUpdated = repository.save(client);
+
+        return ResponseEntity.ok(clientUpdated);
+        
     }
     
 }
