2e677cd5a3f197f364e00d54dc6f9899def1d991
Luan Teixeira <luanlts@outlook.com>
2025-03-19 22:45:28 -0300

    feat(components): add cabecalho

--- ui-angular/src/app/app.module.ts
+++ ui-angular/src/app/app.module.ts
@@ -5,12 +5,15 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HomeComponent } from './components/home/home.component'
 import { AboutComponent } from './components/about/about.component';
-
-
-import {MatToolbarModule} from '@angular/material/toolbar';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { HomeComponent } from './components/home/home.component'
+
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 
 @NgModule({
   declarations: [
@@ -23,6 +26,7 @@ import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    MatButtonModule,
     MatToolbarModule
   ],
   providers: [],
