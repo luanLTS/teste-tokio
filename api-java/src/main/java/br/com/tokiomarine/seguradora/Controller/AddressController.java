2ef884df74406bf62cf8965816f5c1dfcc11c996
Luan Teixeira <luanlts@outlook.com>
2025-03-19 23:29:56 -0300

    fix(Controller): add anotacao @CrossOrigin
    adiciona a anotacao @CrossOrigin para aceitar requisicoes de outras origens e nao ter erro de cors no frontend

--- api-java/src/main/java/br/com/tokiomarine/seguradora/Controller/AddressController.java
+++ api-java/src/main/java/br/com/tokiomarine/seguradora/Controller/AddressController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import br.com.tokiomarine.seguradora.repository.IClientRepository;
 import br.com.tokiomarine.seguradora.service.AddressService;
 import br.com.tokiomarine.seguradora.utils.Utils;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/address")
 public class AddressController {
--- api-java/src/main/java/br/com/tokiomarine/seguradora/Controller/ClientController.java
+++ api-java/src/main/java/br/com/tokiomarine/seguradora/Controller/ClientController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import br.com.tokiomarine.seguradora.repository.IAddressRepository;
 import br.com.tokiomarine.seguradora.repository.IClientRepository;
 import br.com.tokiomarine.seguradora.utils.Utils;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/client")
 public class ClientController {
